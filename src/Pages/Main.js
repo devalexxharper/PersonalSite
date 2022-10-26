d54445d6d6db334fc5ca387b675146000c8b60d4
devalexxharper <alexxharperbr@icloud.com>
2022-10-25 22:58:54 -0300

    Refreshing develop

---
+++ src/Pages/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from '../Components/Container'
+
+export default function Main() {
+  return (
+    <>
+      <Container />
+    </>
+  )
+}
---
+++ test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Icon } from '@iconify/react'
+
+export default function test() {
+  return (
+    <>
+      <Icon icon=" " />
+    </>
+  )
+}
